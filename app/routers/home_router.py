5d3b51505f2ef7cc734a9b9f8e37c09ebc0b2cf3
Martin Sang Do <dothanhsang1984@yahoo.com>
2026-04-22 05:39:32 +0700

    - Display list to website

--- app/routers/home_router.py
+++ app/routers/home_router.py
@@ -1,9 +1,14 @@
 from fastapi import APIRouter, Request
 from fastapi.templating import Jinja2Templates
+from app.services.user_service import user_service
 
 router = APIRouter(prefix="/home")
 templates = Jinja2Templates(directory="app/templates")
 
 @router.get("/csv")
 async def show_csv(request: Request):
-    return templates.TemplateResponse("csv_analysis.html", {"request": request})
+    content = {"request": request}
+    content['username'] = "Diego Maradona"
+    users = await user_service.get_all_users()
+    content['user_list'] = users
+    return templates.TemplateResponse("csv_analysis.html", content)
