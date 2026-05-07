56af63d742e411b767c3495af871572f8e6055fe
Martin Sang Do <dothanhsang1984@yahoo.com>
2026-05-07 16:46:22 +0700

    - delete comment

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -59,7 +59,6 @@ class UserService:
         db = get_database()
         # DANGEROUS: Using raw strings/dict with unvalidated input
         # In MongoDB, a user could pass {"$ne": null} as the username via JSON
-        #query = f"{{'username': '{username}'}}" 
         print(username)
         user_raw = await db[self.collection_name].find_one({"username": username})
         return UserResponse(**user_raw) if user_raw else None
